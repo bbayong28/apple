6ecfdc850645e784994476e83356e956672bb16f
bbayong28 <https://github.com/bbayong28/hospital_tr.git>
2022-11-21 15:52:13 +0900

    20221121

--- level3/lecture1/main.js
+++ level3/lecture1/main.js
@@ -11,9 +11,22 @@ $('.list').click(function (e) {
     탭열기(e.target.dataset.id)
 });
 
-var car = '소나타';
-var price = 50000;
-var color = 'white';
+//var car = '소나타';
+//var price = 50000;
+//var color = 'white';
 //변수 하나에 여러자료 저장 가능
-var c
-document.querySelector('.car_name')[0].innerHTML = car;
+//var car = '소나타 50000 white'
+//but 꺼내쓰기 어려움 => 그래서 [] 배열(array)로 만들어 줌
+//array쓰면 여러자료를 변수 하나에 저장가능
+//var car = ['소나타', 50000, 'white']
+//car[0] = '벤츠' // 배열 수정
+//car[3] = '벤츠' // 배열 추가
+//console.log(car[0])
+//document.querySelector('.car_name')[0].innerHTML = car;
+
+//객체 object자료형
+//var car2 = { name: '소나타', price: 50000, color: 'white' }
+//car2.name = '벤츠' // 배열 수정
+//car2.tire = 4 // 배열 추가
+//console.log(car2.name)
+//console.log(car2['name'])
