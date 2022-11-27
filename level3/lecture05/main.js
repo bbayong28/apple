212cd87b94afb2daebaa32f0b4440e2ab23e93f6
bbayong28 <https://github.com/bbayong28/hospital_tr.git>
2022-11-28 01:24:02 +0900

    20221128

--- level3/lecture05/main.js
+++ level3/lecture05/main.js
@@ -24,11 +24,51 @@ $('.list').click(function (e) {
 //<script> 안에 대충 적은 코드는 페이지 로드 시 1회만 실행됨.
 //유저가 선택할 때마다 이 코드 실행하게 만들어야 함
 //input이벤트 : 유저가 <input> 값을 변경할 때 input 이벤트 발동!!
-$('.form-select').eq(0).on('input', function (e) { 
+$('.form-select').eq(0).on('input', function (e) {
     var value = e.currentTarget.value;
     //var value = this.value;
     //var value = $('.form-select').eq(0).val();
-    if(value == '셔츠'){
+    if (value == '셔츠') {
         $('.form-select').eq(1).removeClass('form-hide')
+    } else if (value == '바지') {        
+        $('.form-select').eq(1).removeClass('form-hide');
+        $('.form-select').eq(1).html('');
+        var pantsSize = `
+            <option>사이즈를선택하세요.</option>
+            <option>28</option>
+            <option>30</option>
+        `;
+        $('.form-select').eq(1).append(pantsSize);
     }
-})
+});
+
+var a = document.createElement('p');
+a.innerHTML = '안녕';
+//a.classList.add('make');
+//test라는 아이디를 가진 요소 안에 a를 만듦.
+//만약 test라는 요소안에 다른 html요소가 있으면 하단에 추가해줌
+document.querySelector('#test').appendChild(a);
+
+var 템플릿 = '<p>안녕</p>';
+//js
+document.querySelector('#test').insertAdjacentHTML('beforeend', 템플릿);
+document.querySelector('#test').innerHTML = 템플릿;
+//jquery
+$('#test').append(템플릿);
+$('#test').html(템플릿);
+
+//바지선택하면 28,30옵션 나오게 하려면?
+//document.querySelector('.form-select').querySelectorAll[3].//on('input', function (e) {
+//    var pantsSize = e.currentTarget.value;
+//    var 사이즈 =`
+//        <select>
+//            <option>사이즈를 선택하세요.</option>
+//            <option>28</option>
+//            <option>30</option>
+//        </select>`
+//    if (pantsSize == '바지') {
+//        $('#test').append(사이즈);
+//    }
+// })
+
+//바지를 눌렀다가 셔츠를 누르면 이상해짐 (알아서 해결)
