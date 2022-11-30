7757192c47f57a1297179678ad8c310ef3405189
apple <malsoonie@MacBook-Pro.local>
2022-12-01 01:10:50 +0900

    20221201

--- level3/lecture09/main.js
+++ level3/lecture09/main.js
@@ -5,15 +5,51 @@ var products = [
 ];
 
 //내가 한 방법
-$('.row').eq(0).append('<div class="col-sm-4">')
-$('.col-sm-4').eq(0).append('<img src="https://via.placeholder.com/600" class="w-100">')
-$('.col-sm-4').eq(0).append(`<h5>${products[0].title}</h5>`)
-$('.col-sm-4').eq(0).append(`<p>가격 : ${products[0].price}</p>`)
-$('.row').eq(0).append('<div class="col-sm-4">')
-$('.col-sm-4').eq(1).append('<img src="https://via.placeholder.com/600" class="w-100">')
-$('.col-sm-4').eq(1).append(`<h5>${products[1].title}</h5>`)
-$('.col-sm-4').eq(1).append(`<p>가격 : ${products[1].price}</p>`)
-$('.row').eq(0).append('<div class="col-sm-4">')
-$('.col-sm-4').eq(2).append('<img src="https://via.placeholder.com/600" class="w-100">')
-$('.col-sm-4').eq(2).append(`<h5>${products[2].title}</h5>`)
-$('.col-sm-4').eq(2).append(`<p>가격 : ${products[2].price}</p>`)
+//$('.row').eq(0).append('<div class="col-sm-4">')
+//$('.col-sm-4').eq(0).append('<img src="https://via.placeholder.com/600" class="w-100">')
+//$('.col-sm-4').eq(0).append(`<h5>${products[0].title}</h5>`)
+//$('.col-sm-4').eq(0).append(`<p>가격 : ${products[0].price}</p>`)
+//$('.row').eq(0).append('<div class="col-sm-4">')
+//$('.col-sm-4').eq(1).append('<img src="https://via.placeholder.com/600" class="w-100">')
+//$('.col-sm-4').eq(1).append(`<h5>${products[1].title}</h5>`)
+//$('.col-sm-4').eq(1).append(`<p>가격 : ${products[1].price}</p>`)
+//$('.row').eq(0).append('<div class="col-sm-4">')
+//$('.col-sm-4').eq(2).append('<img src="https://via.placeholder.com/600" class="w-100">')
+//$('.col-sm-4').eq(2).append(`<h5>${products[2].title}</h5>`)
+//$('.col-sm-4').eq(2).append(`<p>가격 : ${products[2].price}</p>`)
+
+
+var 템플릿 =
+    `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${products[0].title}</h5>
+        <p>가격 : ${products[0].price}</p>
+    </div>`;
+    $('.row').append(템플릿)
+var 템플릿 =
+    `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${products[1].title}</h5>
+        <p>가격 : ${products[1].price}</p>
+    </div>`;
+    $('.row').append(템플릿)
+var 템플릿 =
+    `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${products[2].title}</h5>
+        <p>가격 : ${products[2].price}</p>
+    </div>`;
+    $('.row').append(템플릿);
+
+products.forEach((data, i)=>{
+    `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${data[i].title}</h5>
+        <p>가격 : ${data[i].price}</p>
+    </div>`;
+    $('.row').append(템플릿);
+
+
+})
+    
+
