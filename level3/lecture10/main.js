35eacb99a6de7ce149756ec793c5b8ebc32cd1ae
apple <ghkdud5795@hanmail.net>
2022-12-01 23:41:17 +0900

    20221201

--- level3/lecture10/main.js
+++ level3/lecture10/main.js
@@ -4,54 +4,52 @@ var products = [
     { id: 2, price: 60000, title: 'Black Monastery' }
 ];
 
-//내가 한 방법
-//$('.row').eq(0).append('<div class="col-sm-4">')
-//$('.col-sm-4').eq(0).append('<img src="https://via.placeholder.com/600" class="w-100">')
-//$('.col-sm-4').eq(0).append(`<h5>${products[0].title}</h5>`)
-//$('.col-sm-4').eq(0).append(`<p>가격 : ${products[0].price}</p>`)
-//$('.row').eq(0).append('<div class="col-sm-4">')
-//$('.col-sm-4').eq(1).append('<img src="https://via.placeholder.com/600" class="w-100">')
-//$('.col-sm-4').eq(1).append(`<h5>${products[1].title}</h5>`)
-//$('.col-sm-4').eq(1).append(`<p>가격 : ${products[1].price}</p>`)
-//$('.row').eq(0).append('<div class="col-sm-4">')
-//$('.col-sm-4').eq(2).append('<img src="https://via.placeholder.com/600" class="w-100">')
-//$('.col-sm-4').eq(2).append(`<h5>${products[2].title}</h5>`)
-//$('.col-sm-4').eq(2).append(`<p>가격 : ${products[2].price}</p>`)
+////간단한 array 자료 정렬하는 법
+//var 숫자배열 = [7, 3, 5, 2, 40];
+//var 문자배열 = ['a', 'c', 'b']
+////sort() 함수는 문자정렬
+//숫자배열.sort();
+////console.log(숫자배열)// 2,3,40,5,7 로 정렬함
+////숫자 오름차순 정렬 
+//// 1. a, b는 array안의 자료임
+//// 2. return 오른쪽이 양수면 a를 오른쪽으로
+//// 3. return 오른쪽이 음수면 a를 왼쪽으로
+//숫자배열.sort(function (a, b) {
+//    return a - b
+//});
+////console.log(숫자배열)// 2,3,5,7,40 로 정렬함
+//
+////숫자 내림차순 정렬 
+//숫자배열.sort(function (a, b) {
+//    return b - a
+//});
+////console.log(숫자배열)// 40,7,5,3,2 로 정렬함
+//
+//
+////문자 가나다 순 정렬하기
+//문자배열.sort();
+//console.log(문자배열)
+//
+////문자 다나가 순 정렬
+//문자배열.sort(function (a, b) {
+//    if (a > b) {
+//        return -1
+//    } else if (a < b) {
+//        return 1
+//    } else {
+//        return 0
+//    }
+//});
+//console.log(문자배열)
+//
+////문자 다나가 순 정렬
+//문자배열.reverse();
+//console.log(문자배열)
 
 
-//var 템플릿 =
-//    `<div class="col-sm-4">
-//        <img src="https://via.placeholder.com/600" class="w-100">
-//        <h5>${products[0].title}</h5>
-//        <p>가격 : ${products[0].price}</p>
-//    </div>`;
-//    $('.row').append(템플릿)
-//var 템플릿 =
-//    `<div class="col-sm-4">
-//        <img src="https://via.placeholder.com/600" class="w-100">
-//        <h5>${products[1].title}</h5>
-//        <p>가격 : ${products[1].price}</p>
-//    </div>`;
-//    $('.row').append(템플릿)
-//var 템플릿 =
-//    `<div class="col-sm-4">
-//        <img src="https://via.placeholder.com/600" class="w-100">
-//        <h5>${products[2].title}</h5>
-//        <p>가격 : ${products[2].price}</p>
-//    </div>`;
-//    $('.row').append(템플릿);
 
-//products.forEach((asssss, i) => {
-//    var 템플릿 =
-//        `<div class="col-sm-4">
-//            <img src="https://via.placeholder.com/600" class="w-100">
-//            <h5>${products[i].title}</h5>
-//            <p>가격 : ${products[i].price}</p>
-//        </div>`;
-//    $('.row').append(템플릿);
-//})
 
-//위랑 같음
+
 products.forEach((asssss, i) => {
     var 템플릿 =
         `<div class="col-sm-4">
@@ -62,9 +60,6 @@ products.forEach((asssss, i) => {
     $('.row').append(템플릿);
 })
 
-
-//더보기 버튼 누르면 상품 3개 가져와서 출력하기
-//https://codingapple1.github.io/js/more1.json get요청하면 상품3개 더 줌
 $('#more').on('click', function () {
     $.get('https://codingapple1.github.io/js/more1.json').done(function (data) {
         //console.log(data);
