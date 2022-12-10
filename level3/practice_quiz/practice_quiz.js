d0e55ce3565a904fc2f1cc3c24269c73b6f67106
apple <ghkdud5795@hanmail.net>
2022-12-10 19:26:58 +0900

    20221210

--- level3/practice_quiz/practice_quiz.js
+++ level3/practice_quiz/practice_quiz.js
@@ -1,15 +1,11 @@
+//$.getJSON("store.json", function (data) {
+//  console.log(data.products[0].title);
+//
+//});
 
-//https://www.delftstack.com/ko/howto/javascript/load-json-file-in-javascript/
-//블로그 방법 1
-//fetch("./students.json").then(response => {
-//  return response.json();
-//}).then(jsondata => console.log(jsondata));
-//블로그 방법 2
-//const jsonData= require('./students.json'); 
-//console.log(jsonData);
+let products = [];
+let cart = [];
 
-
-$.getJSON("store.json", function (data) {
-  console.log(data.products[0].title);
-
-});
+$.get('store.json').then((data) => { 
+  products = data.products
+})
