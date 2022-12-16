573a883b631792274ec6dcb1e27217fc6d9d947d
apple <ghkdud5795@hanmail.net>
2022-12-16 15:54:16 +0900

    20221216

--- react/level1/src/App.js
+++ react/level1/src/App.js
@@ -1,17 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-
+  //자료 잠깐 저장할 땐 변수
+  //let post = '강남 우동 맛집';
+  //react에서 state 써도 됨
   let post = '강남 우동 맛집';
-  document.querySelector('h4').innerHTML = post;
+  //b는 state 변경을 도와주는 함수
+  //let [글제목a, a] = useState('남자 코트 추천')
+  //let [글제목b, b] = useState('강남 우동 맛집')
+  //let [글제목c, c] = useState('파이썬 독학')
+  let [글제목d, d] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
+
+
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임.</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+
+      {/*       <div className="list">
+        <h4>{글제목a}</h4>
+        <p>12월 16일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목b}</h4>
+        <p>12월 16일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목c}</h4>
+        <p>12월 16일 발행</p>
+      </div> */}
+      <div className="list">
+        <h4>{글제목d[0]}</h4>
+        <p>12월 16일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목d[1]}</h4>
+        <p>12월 16일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목d[2]}</h4>
+        <p>12월 16일 발행</p>
+      </div>
     </div>
   );
 }
