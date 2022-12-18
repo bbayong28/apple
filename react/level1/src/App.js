90af6050f3f5af4298b58a23cf1e9341097f33ea
apple <ghkdud5795@hanmail.net>
2022-12-18 17:51:48 +0900

    20221218

--- react/level1/src/App.js
+++ react/level1/src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+/* eslint-disable */
+//터미널 WARNING message끄는 거
+
 import './App.css';
 import { useState } from 'react'
 
@@ -8,10 +10,10 @@ function App() {
   //react에서 state 써도 됨
   let post = '강남 우동 맛집';
   //b는 state 변경을 도와주는 함수
-  //let [글제목a, a] = useState('남자 코트 추천')
-  //let [글제목b, b] = useState('강남 우동 맛집')
-  //let [글제목c, c] = useState('파이썬 독학')
-  let [글제목d, d] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
+  const [like, setLike] = useState(0);
+  const [ch, setCh] = useState('남자 코트 추천');
+
 
 
 
@@ -21,32 +23,37 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      {/*       <div className="list">
-        <h4>{글제목a}</h4>
+      {/*   
+      <button onClick={() => {
+        글제목[0] = '여자 코트 추천';
+        글제목변경(글제목)
+      }}>글수정</button> 
+      
+      (참고)
+      array/object 다룰 때 원본은 보존하는게 좋음.
+      글제목[0] = '여자 코트 추천'; 이렇게하면 영구적으로 수정해버린 것.
+      */}
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+
+      <div className="list">
+        <h4>{글제목[0]} <span onClick={() => { setLike(like + 1) }}>👍🏻</span> {like} </h4>
         <p>12월 16일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목b}</h4>
+        <h4>{글제목[1]}</h4>
         <p>12월 16일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목c}</h4>
-        <p>12월 16일 발행</p>
-      </div> */}
-      <div className="list">
-        <h4>{글제목d[0]}</h4>
+        <h4>{글제목[2]}</h4>
         <p>12월 16일 발행</p>
       </div>
-      <div className="list">
-        <h4>{글제목d[1]}</h4>
-        <p>12월 16일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목d[2]}</h4>
-        <p>12월 16일 발행</p>
-      </div>
-    </div>
+    </div >
   );
 }
+
 
 export default App;
