a4dafb3098df69941831c73c441e17e34c8115e0
apple <ghkdud5795@hanmail.net>
2023-01-09 03:05:25 +0900

    20230109

--- react/level2/src/App.js
+++ react/level2/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+//import bg from './img/main-bg.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">JShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#furniture">Furniture</Nav.Link>
+            <Nav.Link href="#light">Light</Nav.Link>
+            <Nav.Link href="#homedeco">HomeDeco</Nav.Link>
+            <Nav.Link href="#gardening">Gardening</Nav.Link>
+            <Nav.Link href="#etc">Etc.</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      {/* 메인배경 넣기 */}
+      {/* <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div> */}
+      <div className='main-bg'></div>
+
+      {/* 상품 레이아웃 3개 - bootstrap사용 */}
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img src={process.env.PUBLIC_URL + 'logo192.png'} width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>          
+        </div>
+      </div>
+
+
     </div>
   );
 }
