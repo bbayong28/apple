68f238dc8de69586bdaadc64d35d0eda038950b7
apple <ghkdud5795@hanmail.net>
2023-01-09 16:02:02 +0900

    20230109

--- react/level2/src/App.js
+++ react/level2/src/App.js
@@ -1,8 +1,41 @@
+/* src속 img 삭제해도됨 */
+
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data';
 //import bg from './img/main-bg.png'
 
+
 function App() {
+/* 코드가 너무 길고 복잡해져서 data.js에 넣고 가져와서 쓰기! */
+/* let [shoes, setShoes] = useState([
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
+) */
+  
+  let [shoes] = useState(data);
+  console.log(shoes[0])
+
   return (
     <div className="App">
 
@@ -26,27 +59,59 @@ function App() {
       {/* 상품 레이아웃 3개 - bootstrap사용 */}
       <div className='container'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img src={process.env.PUBLIC_URL + 'logo192.png'} width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+          {/* <div className='col-md-4'>
+            {/* <img src={process.env.PUBLIC_URL + 'logo192.png'} width="80%"/> 
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+            <h4>{shoes[0].title}</h4>
+            <h4>{shoes[0].price}</h4>
           </div>
           <div className='col-md-4'>
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>            
+            <h4>{shoes[1].title}</h4>
+            <h4>{shoes[1].price}</h4>
           </div>
           <div className='col-md-4'>
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>          
-        </div>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>            
+            <h4>{shoes[2].title}</h4>
+            <h4>{shoes[2].price}</h4>
+          </div>  */}    
+          
+          {/* 
+          <Product shoes={shoes[0]} i={1}></Product>
+          <Product shoes={shoes[1]} i={2}></Product>
+          <Product shoes={shoes[2]} i={3}></Product> 
+          */}
+
+          {
+            shoes.map((a, i) => { 
+              return (
+                <Product shoes={shoes[i]} i={i}></Product>
+              )
+            })
+          }
+          
+        </div>           
       </div>
-
-
     </div>
   );
 }
 
+function Product(props){ 
+  return (
+    <div className='col-md-4'>
+      {/* <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" /> */}
+      <img src={process.env.PUBLIC_URL + './img/product0' + (props.i + 1) + '.png'}
+      width="80%"/>
+      <h4>{props.shoes.title}</h4>
+      <h4>{props.shoes.price}</h4>
+    </div>
+  )
+}
+
+
+
 export default App;
+
+
+
+/* 상품목록 컴포넌트화 , 데이터바인딩도 좀 달라지니까 잘 고쳐오기, 반복적인 부분 map 반복문 써보기 */
---
+++ react/level2/src/data.js
@@ -0,0 +1,34 @@
+let data = [
+  {
+    id : 0,
+    title : "Wide Set Red",
+    content : "HIGHALU",
+    price : '1,709,000'
+  },
+
+  {
+    id : 1,
+    title : "Wide Set Ivory",
+    content : "HIGHALU",
+    price : '1,749,000'
+  },
+
+  {
+    id : 2,
+    title : "Wide Set Grren",
+    content : "HIGHALU",
+    price : '1,729,000'
+  }
+] 
+
+
+export default data;
+  
+  
+  
+  
+
+
+
+
+
