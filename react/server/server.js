929fef2f30528e697194628582d8f41d1cbd8c29
apple <ghkdud5795@hanmail.net>
2023-01-24 11:07:20 +0900

    20230124

--- react/server/server.js
+++ react/server/server.js
@@ -6,7 +6,7 @@ app.listen(8080, function () {
   console.log('listening on 8080')
 }); 
 
-//이거 추가해놔야 ajax 잘됨
+//이거 세 줄 추가해놔야 ajax 잘됨
 app.use(express.json());
 var cors = require('cors');
 app.use(cors());
@@ -20,17 +20,20 @@ app.get('/', function (요청, 응답) {
   응답.sendFile(path.join(__dirname, 'react_server/build/index.html'))
 })
 
+
 //예 - DB에 있던 상품명을 보여주려면?
+//이경로로 GET 요청을 하면 DB에서 데이터를 뽑아서 유저한테 보내주세요
 //리액트 파일에서 상품데이터 필요하면 /product로 GET 요청하면 끝
 //근데 이거 쓰려면 위에 세 줄 추가해놔야함.
-app.get('/product', function (요청, 응답) {   
-  응답.json({name : 'black shoes'})
+app.get('/product', function (요청, 응답) { 
+  //데이터를 보내주고 싶으면
+  응답.json({ name: 'black shoes' })
 })
+
 
 
 //리액트 라우터 쓰는경우 추가해야할 문법
 //유저가 주소창에 이상한 경로를 입력했을 때, 서버에 기능 개발이 안되어 있으면 리액트로 만든 html 페이지 보여줘라.
-
 app.get('*', function (요청, 응답) { 
   응답.sendFile(path.join(__dirname, 'react_server/build/index.html'))
 })
